0fcf07dbad6fda4eca07d9053bc444a481662287
ivonnm <66538222+ivonnm@users.noreply.github.com>
2021-05-16 17:48:27 -0500

    Tarea

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+ public title:string = 'Contador con angular';
+ public numero:number = 0;
+
+ public sumarRestar(valor:number){
+   this.numero = this.numero + valor;
+
+ }
+}
+
+
